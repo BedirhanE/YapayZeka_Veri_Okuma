44468d9dcee370310524d844e4a4232515ab4a77
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-11-16 18:36:40 +0300

    Update seaborn_veri_gorselleştirme.py

--- seaborn_veri_gorselleştirme.py
+++ seaborn_veri_gorselleştirme.py
@@ -2,5 +2,31 @@
 #veri görselleştirme işlemleri
 
 import seaborn as sns
+import matplotlib.pyplot as plt
+
 planets =sns.load_dataset("planets")
 print(planets.head())
+
+
+
+
+# Veri setini yükle
+planets = sns.load_dataset("planets")
+
+# Veri setinin ilk birkaç satırını yazdır
+print(planets.head())
+
+# Scatter plot (Nokta Grafiği)
+sns.scatterplot(x='orbital_period', y='mass', data=planets)
+plt.title('Orbital Period vs Mass')
+plt.show()
+
+# Box plot (Kutu Grafiği)
+sns.boxplot(x='year', y='orbital_period', data=planets)
+plt.title('Orbital Period by Year')
+plt.show()
+
+# Histogram
+sns.histplot(x='distance', data=planets, bins=20, kde=True)
+plt.title('Distribution of Distances')
+plt.show()
